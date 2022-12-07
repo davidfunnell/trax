24addcf05cb0dd095e1189bc3a2087d3f23c594e
David Funnell <dfunnell77@gmail.com>
2022-12-06 18:21:14 -0800

    Technician models/views/urls work, Service models/(get/post)views/(get/post)urls work.

--- service/api/service_rest/models.py
+++ service/api/service_rest/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
+
+class InventoryVinsVO(models.Model):
+    vin_vo = models.CharField(max_length=17, unique=True)
+
+
+class Technician(models.Model):
+    name = models.CharField(max_length=100)
+    employee_number = models.PositiveSmallIntegerField(unique=True)
+
+    def get_api_url(self):
+        return reverse("api_manufacturer", kwargs={"pk": self.id})
+
+
+class ServiceAppointment(models.Model):
+    vin = models.CharField(max_length=17)
+    owner_name = models.CharField(max_length=100)
+    date = models.DateField()
+    time = models.TimeField()
+    description = models.TextField()
+    purchased = models.BooleanField()
+    technician = models.ForeignKey(
+        Technician,
+        related_name="technician",
+        on_delete=models.CASCADE,
+    )
+
+    def get_api_url(self):
+        return reverse("api_vehicle_model", kwargs={"pk": self.id})
--- service/api/service_rest/views.py
+++ service/api/service_rest/views.py
@@ -1,3 +1,126 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+from django.views.decorators.http import require_http_methods
+import json
+from common.json import ModelEncoder
+from .models import InventoryVinsVO, Technician, ServiceAppointment
+
 
 # Create your views here.
+
+class TechnicianEncoder(ModelEncoder):
+    model = Technician
+    properties = [
+        "name",
+        "employee_number",
+        "id",
+    ]
+
+class ServiceAppointmentsListEncoder(ModelEncoder):
+    model = ServiceAppointment
+    properties = [
+        "vin",
+        "owner_name",
+        "date",
+        "time",
+        "description",
+        "purchased",
+        "id",
+        "technician",
+
+    ]
+    encoders = {
+        "technician": TechnicianEncoder(),
+    }
+
+
+#This function gets both hats by locations and all hats
+#This function allows you to create a hat with a locationVO
+@require_http_methods(["GET", "POST"])
+def api_list_service_appointments(request, vin=None):
+    if request.method == "GET":
+        if vin is not None:
+            service_appointments = ServiceAppointment.objects.filter(vin=vin)
+        else:
+            service_appointments = ServiceAppointment.objects.all()
+        return JsonResponse(
+            {"service appointments": service_appointments},
+            encoder=ServiceAppointmentsListEncoder,
+        )
+    else:
+        content = json.loads(request.body)
+        # delete purchased true and implement below once I get polling working.
+        # try:
+        #     purchased = InventoryVinsVO.objects.get(vin_vo=content["vin"])
+        #     content["purchased"] = True
+        # except InventoryVinsVO.DoesNotExist:
+        #     content["purchased"] = False
+
+        try:
+            content["purchased"] = True
+            technician = Technician.objects.get(id=content["technician"])
+            content["technician"] = technician
+            service = ServiceAppointment.objects.create(**content)
+            return JsonResponse(
+                service,
+                encoder=ServiceAppointmentsListEncoder,
+                safe=False,
+            )
+        except:
+            response = JsonResponse(
+                {"message": "Could not create the Service Appointment"}
+            )
+            response.status_code = 400
+            return response
+
+
+
+
+#This function allows you to get the details of a hat or delete it
+# @require_http_methods(["GET", "DELETE"])
+# def api_show_hat(request, pk):
+#     if request.method == "GET":
+#         try:
+#             hat = Hat.objects.get(id=pk)
+#             return JsonResponse(
+#                 hat,
+#                 encoder=HatDetailEncoder,
+#                 safe=False
+#             )
+#         except Hat.DoesNotExist:
+#             response = JsonResponse({"message": "Does not exist, Can't Get Detail View"})
+#             response.status_code = 404
+#             return response
+#     elif request.method == "DELETE":
+#         try:
+#             hat = Hat.objects.get(id=pk)
+#             hat.delete()
+#             return JsonResponse({"deleted": "true"})
+#         except Hat.DoesNotExist:
+#             return JsonResponse({"message": "Does not exist, Can't Delete"})
+
+
+#added this so i can use it for my dropdown list to create a new hat based off of VO locations
+@require_http_methods(["GET", "POST"])
+def api_list_technicians(request):
+    if request.method == "GET":
+        technicians = Technician.objects.all()
+        return JsonResponse(
+            {"technicians": technicians},
+            encoder=TechnicianEncoder,
+        )
+    else:
+        try:
+            content = json.loads(request.body)
+            technician = Technician.objects.create(**content)
+            return JsonResponse(
+                technician,
+                encoder=TechnicianEncoder,
+                safe=False,
+            )
+        except:
+            response = JsonResponse(
+                {"message": "Could not create the Technician"}
+            )
+            response.status_code = 400
+            return response
