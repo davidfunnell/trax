bb301d773e89a975dee0063dfa4bdc1658938735
David Funnell <dfunnell77@gmail.com>
2022-12-07 11:04:33 -0800

    Create a technician form works

--- ghi/app/src/TechnicianForm.js
+++ ghi/app/src/TechnicianForm.js
@@ -11,57 +11,53 @@ class TechnicianForm extends React.Component {
     };
     this.handleNameChange = this.handleNameChange.bind(this);
     this.handleEmployeeNumberChange = this.handleEmployeeNumberChange.bind(this);
-    // this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
-  //   async handleSubmit(event) {
-  //     event.preventDefault();
-  //     const data = {...this.state};
-  //     data.picture_url = data.pictureUrl;
-  //     delete data.pictureUrl;
-  //     delete data.locations;
-  //     delete data.hasSignedUp
-  //     console.log(data)
+    async handleSubmit(event) {
+      event.preventDefault();
+      const data = {...this.state};
+      data.employee_number = data.employeeNumber;
+      delete data.employeeNumber;
+      delete data.created
+      console.log(data)
 
-  //     const locationUrl = 'http://localhost:8090/api/hats/';
-  //     const fetchConfig = {
-  //       method: "post",
-  //       body: JSON.stringify(data),
-  //       headers: {
-  //         'Content-Type': 'application/json',
-  //       },
-  //     };
-  //     const response = await fetch(locationUrl, fetchConfig);
-  //     if (response.ok) {
-  //       const newHat = await response.json();
-  //       console.log(newHat);
+      const techniciansUrl = 'http://localhost:8080/api/technicians/';
+      const fetchConfig = {
+        method: "post",
+        body: JSON.stringify(data),
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      };
+      const response = await fetch(techniciansUrl, fetchConfig);
+      if (response.ok) {
+        const newTechnician = await response.json();
+        console.log(newTechnician);
 
-  //       const cleared = {
-  //         fabric: '',
-  //         style: '',
-  //         color: '',
-  //         pictureUrl: '',
-  //         location: '',
-  //         hasSignedUp: true,
-  //       };
-  //       this.setState(cleared);
-  //     }
-  //   }
+        const cleared = {
+          name: '',
+          employeeNumber: '',
+          created: true,
+        };
+        this.setState(cleared);
+      }
+    }
 
   handleNameChange(event) {
     const value = event.target.value;
-    this.setState({ name: value, hasSignedUp: false })
+    this.setState({ name: value, created: false })
   }
 
   handleEmployeeNumberChange(event) {
     const value = event.target.value;
-    this.setState({ employeeNumber: value, hasSignedUp: false })
+    this.setState({ employeeNumber: value, created: false })
   }
 
   render() {
     let messageClasses = 'alert alert-success d-none mb-0';
     let formClasses = '';
-    if (this.state.hasSignedUp) {
+    if (this.state.created) {
       messageClasses = 'alert alert-success mt-3 mb-0';
     }
     return (
@@ -81,7 +77,7 @@ class TechnicianForm extends React.Component {
                 <button className="btn btn-primary">Create</button>
               </form>
               <div className={messageClasses} id="success-message">
-                Congratulations! You created a hat!
+                Congratulations! You created a technician!
               </div>
             </div>
           </div>
