60e690b80f17462232542221404ff28e7722da20
David Funnell <dfunnell77@gmail.com>
2022-12-07 11:45:35 -0800

    Updated service form with all correct info, drop down working, need to update handlesubmit

--- ghi/app/src/ServiceForm.js
+++ ghi/app/src/ServiceForm.js
@@ -1,146 +1,156 @@
 import React from 'react';
 
 class ServiceForm extends React.Component {
-    // constructor(props) {
-    //     super(props)
-    //     this.state = {
-    //       fabric: '',
-    //       style: '',
-    //       color: '',
-    //       pictureUrl: '',
-    //       location: '',
-    //       hasSignedUp: false,
-    //       locations: [],
-    //     };
-    //     this.handleFabricChange = this.handleFabricChange.bind(this);
-    //     this.handleStyleChange = this.handleStyleChange.bind(this);
-    //     this.handleColorChange = this.handleColorChange.bind(this);
-    //     this.handlePictureUrlChange = this.handlePictureUrlChange.bind(this);
-    //     this.handleLocationChange = this.handleLocationChange.bind(this);
-    //     this.handleSubmit = this.handleSubmit.bind(this);
-    //   }
+  constructor(props) {
+    super(props)
+    this.state = {
+      vin: '',
+      ownerName: '',
+      date: '',
+      time: '',
+      description: '',
+      technician: '',
+      created: false,
+      technicians: [],
+    };
+    this.handleVinChange = this.handleVinChange.bind(this);
+    this.handleOwnerNameChange = this.handleOwnerNameChange.bind(this);
+    this.handleDateChange = this.handleDateChange.bind(this);
+    this.handleTimeChange = this.handleTimeChange.bind(this);
+    this.handleDescriptionChange = this.handleDescriptionChange.bind(this);
+    this.handleTechnicianChange = this.handleTechnicianChange.bind(this);
+    // this.handleSubmit = this.handleSubmit.bind(this);
+  }
 
-    //   async handleSubmit(event) {
-    //     event.preventDefault();
-    //     const data = {...this.state};
-    //     data.picture_url = data.pictureUrl;
-    //     delete data.pictureUrl;
-    //     delete data.locations;
-    //     delete data.hasSignedUp
-    //     console.log(data)
+  //   async handleSubmit(event) {
+  //     event.preventDefault();
+  //     const data = {...this.state};
+  //     data.picture_url = data.pictureUrl;
+  //     delete data.pictureUrl;
+  //     delete data.locations;
+  //     delete data.created
+  //     console.log(data)
 
-    //     const locationUrl = 'http://localhost:8090/api/hats/';
-    //     const fetchConfig = {
-    //       method: "post",
-    //       body: JSON.stringify(data),
-    //       headers: {
-    //         'Content-Type': 'application/json',
-    //       },
-    //     };
-    //     const response = await fetch(locationUrl, fetchConfig);
-    //     if (response.ok) {
-    //       const newHat = await response.json();
-    //       console.log(newHat);
+  //     const locationUrl = 'http://localhost:8090/api/hats/';
+  //     const fetchConfig = {
+  //       method: "post",
+  //       body: JSON.stringify(data),
+  //       headers: {
+  //         'Content-Type': 'application/json',
+  //       },
+  //     };
+  //     const response = await fetch(locationUrl, fetchConfig);
+  //     if (response.ok) {
+  //       const newHat = await response.json();
+  //       console.log(newHat);
 
-    //       const cleared = {
-    //         fabric: '',
-    //         style: '',
-    //         color: '',
-    //         pictureUrl: '',
-    //         location: '',
-    //         hasSignedUp: true,
-    //       };
-    //       this.setState(cleared);
-    //     }
-    //   }
+  //       const cleared = {
+  //         fabric: '',
+  //         style: '',
+  //         color: '',
+  //         pictureUrl: '',
+  //         location: '',
+  //         created: true,
+  //       };
+  //       this.setState(cleared);
+  //     }
+  //   }
 
-    // handleFabricChange(event) {
-    //     const value = event.target.value;
-    //     this.setState({fabric: value, hasSignedUp: false})
-    //     }
+  handleVinChange(event) {
+    const value = event.target.value;
+    this.setState({ vin: value, created: false })
+  }
 
-    // handleStyleChange(event) {
-    //     const value = event.target.value;
-    //     this.setState({style: value, hasSignedUp: false})
-    //     }
+  handleOwnerNameChange(event) {
+    const value = event.target.value;
+    this.setState({ ownerName: value, created: false })
+  }
 
-    // handleColorChange(event) {
-    //     const value = event.target.value;
-    //     this.setState({color: value, hasSignedUp: false})
-    //     }
+  handleDateChange(event) {
+    const value = event.target.value;
+    this.setState({ date: value, created: false })
+  }
 
-    // handlePictureUrlChange(event) {
-    //     const value = event.target.value;
-    //     this.setState({pictureUrl: value, hasSignedUp: false});
-    //     }
+  handleTimeChange(event) {
+    const value = event.target.value;
+    this.setState({ time: value, created: false });
+  }
 
-    // handleLocationChange(event) {
-    //     const value = event.target.value;
-    //     this.setState({location: value, hasSignedUp: false});
-    //     }
+  handleDescriptionChange(event) {
+    const value = event.target.value;
+    this.setState({ description: value, created: false });
+  }
 
-    // async componentDidMount() {
-    //     const url = 'http://localhost:8090/api/locationsvo/';
-
-    //     const response = await fetch(url);
-
-    //     if (response.ok) {
-    //       const data = await response.json();
-    //       this.setState({locations: data.locations});
-    //     }
-    // }
-
-      render() {
-        // let messageClasses = 'alert alert-success d-none mb-0';
-        // let formClasses = '';
-        // if (this.state.hasSignedUp) {
-        //   messageClasses = 'alert alert-success mt-3 mb-0';
-        // }
-        return (
-            <p>Service Form</p>
-        //   <div className="row">
-        //     <div className="offset-3 col-6">
-        //       <div className="shadow p-4 mt-4">
-        //         <h1>Create a new hat</h1>
-        //         <form className={formClasses} onSubmit={this.handleSubmit} id="create-location-form" >
-        //           <div className="form-floating mb-3">
-        //             <input value={this.state.fabric} onChange={this.handleFabricChange} placeholder="fabric" required type="text" name="fabric" id="fabric" className="form-control" />
-        //             <label htmlFor="fabric">Fabric</label>
-        //           </div>
-        //           <div className="form-floating mb-3">
-        //             <input value={this.state.style} onChange={this.handleStyleChange} placeholder="style" required type="text" name="style" id="style" className="form-control" />
-        //             <label htmlFor="style">Style</label>
-        //           </div>
-        //           <div className="form-floating mb-3">
-        //             <input value={this.state.color} onChange={this.handleColorChange} placeholder="color" required type="text" name="color" id="color" className="form-control" />
-        //             <label htmlFor="color">Color</label>
-        //           </div>
-        //           <div className="form-floating mb-3">
-        //             <input value={this.state.pictureUrl} onChange={this.handlePictureUrlChange} placeholder="picture_url" required type="url" name="picture_url" id="picture_url" className="form-control" />
-        //             <label htmlFor="picture_url">Picture URL</label>
-        //           </div>
-        //           <div className="mb-3">
-        //             <select value={this.state.location} onChange={this.handleLocationChange} required name="location" id="location" className="form-select">
-        //               <option value="">Choose a location</option>
-        //               {this.state.locations.map(location => {
-        //                 return (
-        //                   <option key={location.id} value={location.id}>
-        //                     {location.closet_name}
-        //                   </option>
-        //                 );
-        //               })}
-        //             </select>
-        //           </div>
-        //           <button className="btn btn-primary">Create</button>
-        //         </form>
-        //         <div className={messageClasses} id="success-message">
-        //           Congratulations! You created a hat!
-        //         </div>
-        //       </div>
-        //     </div>
-        //   </div>
-        );
+  handleTechnicianChange(event) {
+      const value = event.target.value;
+      this.setState({technician: value, created: false});
       }
+
+  async componentDidMount() {
+      const url = 'http://localhost:8080/api/technicians/';
+
+      const response = await fetch(url);
+
+      if (response.ok) {
+        const data = await response.json();
+        this.setState({technicians: data.technicians});
+      }
+  }
+
+  render() {
+    let messageClasses = 'alert alert-success d-none mb-0';
+    let formClasses = '';
+    if (this.state.created) {
+      messageClasses = 'alert alert-success mt-3 mb-0';
     }
+    return (
+      <div className="row">
+        <div className="offset-3 col-6">
+          <div className="shadow p-4 mt-4">
+            <h1>Create a new service appointment</h1>
+            <form className={formClasses} onSubmit={this.handleSubmit} id="create-location-form" >
+              <div className="form-floating mb-3">
+                <input value={this.state.vin} onChange={this.handleVinChange} placeholder="vin" required type="text" name="vin" id="vin" className="form-control" />
+                <label htmlFor="vin">Vin</label>
+              </div>
+              <div className="form-floating mb-3">
+                <input value={this.state.ownerName} onChange={this.handleOwnerNameChange} placeholder="ownerName" required type="text" name="ownerName" id="ownerName" className="form-control" />
+                <label htmlFor="ownerName">Owners Name</label>
+              </div>
+              <div className="form-floating mb-3">
+                <input value={this.state.date} onChange={this.handleDateChange} placeholder="date" required type="date" name="date" id="date" className="form-control" />
+                <label htmlFor="date">Date</label>
+              </div>
+              <div className="form-floating mb-3">
+                <input value={this.state.time} onChange={this.handleTimeChange} placeholder="time" required type="time" name="time" id="time" className="form-control" />
+                <label htmlFor="time">Time</label>
+              </div>
+              <div className="form-floating mb-3">
+                <textarea value={this.state.description} onChange={this.handleDescriptionChange} placeholder="description" name="description" id="description" className="form-control" rows="3"/>
+                <label htmlFor="description" className="form-label">Description</label>
+              </div>
+              <div className="mb-3">
+                <select value={this.state.technician} onChange={this.handleTechnicianChange} required name="technician" id="technician" className="form-select">
+                  <option value="">Choose a technician</option>
+                  {this.state.technicians.map(technician => {
+                    return (
+                      <option key={technician.id} value={technician.id}>
+                        {technician.name}
+                      </option>
+                    );
+                  })}
+                </select>
+              </div>
+              <button className="btn btn-primary">Create</button>
+            </form>
+            <div className={messageClasses} id="success-message">
+              Congratulations! You created a service appointment!
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
 
 export default ServiceForm;
